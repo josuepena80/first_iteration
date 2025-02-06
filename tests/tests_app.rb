58763ecdd72783329b564f30c8c705cbfb146aba
Josue Pena <josue.pena.business@gmail.com>
2025-02-06 00:12:18 +0000

    done

--- tests/tests_app.rb
+++ tests/tests_app.rb
@@ -1,12 +1,16 @@
-require "minitest/autorun"
-
-require_relative "record_class"
-
 require "date"
+require "minitest/autorun"
+require_relative "/workspaces/first_iteration/record_class.rb"
+require_relative "/workspaces/first_iteration/symptom.rb"
 
-require_relative "symptom"
+class TestRecord < Minitest::Test
+  def setup
+    @record = Record.new  
+  end
 
-def test_end_tracker # should tell whether the puts statement is correct
+  def test_end_tracker # should tell whether the puts statement is correct
   output = capture_io { @record.send(:end_tracker) }.first
   assert_equal "You have closed the Symptom Tracker successfully\n", output
+  end
+
 end
